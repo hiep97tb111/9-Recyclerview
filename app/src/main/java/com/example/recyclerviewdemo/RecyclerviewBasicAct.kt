f2f59793b615d86ae9b4c9adbcac9d0b4b1f99d3
HiepPD <hieppdutck59@gmail.com>
2023-04-29 07:31:39 +0700

    Integrate SwipeRefreshLayout in Recyclerview Basic

--- app/src/main/java/com/example/recyclerviewdemo/RecyclerviewBasicAct.kt
+++ app/src/main/java/com/example/recyclerviewdemo/RecyclerviewBasicAct.kt
@@ -6,14 +6,21 @@ import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
+import androidx.swiperefreshlayout.widget.SwipeRefreshLayout
 import com.example.recyclerviewdemo.adapter.RecyclerviewBasicAdapter
 import com.example.recyclerviewdemo.model.RecyclerviewBasicModel
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
 import java.util.ArrayList
 
 class RecyclerviewBasicAct: AppCompatActivity() {
     private lateinit var listData: ArrayList<RecyclerviewBasicModel>
-    private lateinit var recyclerviewBasic: RecyclerView
     private lateinit var recyclerViewBasicAdapter: RecyclerviewBasicAdapter
+
+    private lateinit var recyclerviewBasic: RecyclerView
+    private lateinit var swipeRefreshLayout: SwipeRefreshLayout
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -27,6 +34,8 @@ class RecyclerviewBasicAct: AppCompatActivity() {
 
     private fun initViews() {
         recyclerviewBasic = findViewById(R.id.recycler_view_basic)
+        swipeRefreshLayout = findViewById(R.id.swipeRefreshLayout)
+
         recyclerViewBasicAdapter = RecyclerviewBasicAdapter(listData){ model, position ->
             Toast.makeText(this, model.counter+"/"+position, Toast.LENGTH_SHORT).show()
         }
@@ -35,14 +44,16 @@ class RecyclerviewBasicAct: AppCompatActivity() {
         recyclerviewBasic.layoutManager = LinearLayoutManager(this)
         recyclerviewBasic.adapter = recyclerViewBasicAdapter
 
-        recyclerviewBasic.addOnScrollListener(object : RecyclerView.OnScrollListener(){
-            override fun onScrolled(recyclerView: RecyclerView, dx: Int, dy: Int) {
-                super.onScrolled(recyclerView, dx, dy)
-                if(! recyclerView.canScrollVertically(1)){
-                    Log.e("Logger", "onScrolled")
-                }
+        swipeRefreshLayout.setOnRefreshListener {
+            // Handle the refresh action here
+            // You can update the content of your view or make a network call to fetch new data
+            // Once the refresh is complete, call swipeRefreshLayout.setRefreshing(false) to stop the animation
+            Toast.makeText(this, "Reload Data", Toast.LENGTH_SHORT).show()
+            CoroutineScope(Dispatchers.Main).launch {
+                delay(4000L)
+                swipeRefreshLayout.isRefreshing = false
             }
-        })
+        }
     }
 
     private fun initData() {
